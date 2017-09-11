49c69466ed1dfb18df2b4620ed87ea3e867f2cd4
availien <availien@dionysus.ba.albany.edu>
2017-09-11 17:46:38 -0400

    N Nearest Stations Mock Func (returns 5, not N)

---
+++ sql/searchFunctions/nearestNStations.sql
@@ -0,0 +1,19 @@
+CREATE FUNCTION search_n_nearest_short_count_stations(
+    q_lon DOUBLE PRECISION,
+    q_lat DOUBLE PRECISION
+  ) RETURNS SETOF VARCHAR AS
+$$
+
+SELECT
+    rc_id
+  FROM short_counts_shp
+  ORDER BY ST_Distance(
+    ST_PointFromText('POINT(' || q_lon || ' ' || q_lat || ')', 4326),
+    ST_PointFromText('POINT(' || longitude || ' ' || latitude || ')', 4326)
+  )
+  LIMIT 5
+
+$$ LANGUAGE SQL STABLE;
+;
+
+
